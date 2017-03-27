1e6a0017fc50306660e84a4bce073292f96afb8e
Artem Shylai <artem_shylai@epam.com>
2017-03-27 23:26:00 +0300

    TDD for new function

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,8 +4,12 @@ export default class App extends React.Component {
     render() {
         return (
             <div style={{textAlign: 'center'}}>
-                <h1>Hello World!</h1>
+                <h1>Hello, {this.getFullName()}!</h1>
             </div>
         );
+    }
+
+    getFullName() {
+        return this.props.name + ' ' + this.props.sername;
     }
 }
--- client/components/App.test.js
+++ client/components/App.test.js
@@ -2,12 +2,31 @@ import React from 'react';
 import App from './App';
 import renderer from 'react-test-renderer';
 
-//Snapshot Testing
-test('render Hellow World text', () => {
-    const component = renderer.create(
-        <App />
-    );
+describe('App', () => {
+    let sut;
+    const name = 'John';
+    const sername = 'Doe';
 
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    beforeEach(() => {
+        sut = new App();
+        sut.props = {
+            name,
+            sername
+        }
+    });
+
+    //Snapshot Testing
+    test('render Hellow World text', () => {
+        const component = renderer.create(
+            <App name='John' sername='Doe' />
+        );
+
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    test('builds user`s full name', () => {
+        expect(sut.getFullName()).toBe(name + ' ' + sername);
+    });
 });
+
