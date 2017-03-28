248eb7c03ebb355d53ad99e72f185abdaddc342d
Artem Shylai <artem_shylai@epam.com>
2017-03-29 00:20:49 +0300

    Setup e2e tests with Nightwatch (http://http://nightwatchjs.org/). Add demo e2e test.

---
+++ e2e-tests/pages/demo.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'Demo test' : function (browser) {
+    browser
+      .url('http://localhost:8080')
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('#root h1', 'Hello')
+      .end();
+  }
+};
